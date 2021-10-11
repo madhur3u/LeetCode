df5f263ef51c6249ccd49e98839148e51f0d4a3b
Madhur Upadhyay <89251393+madhur3u@users.noreply.github.com>
2021-10-11 11:58:38 +0530

    Create 1914 Cyclically_rotating_grid.py

---
+++ 1914 Cyclically_rotating_grid.py
@@ -0,0 +1,101 @@
+# https://leetcode.com/problems/cyclically-rotating-a-grid/
+
+class Solution:
+    def rotateGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        
+        n = len(grid)
+        m = len(grid[0])
+        
+        for shell in range(1, min(n,m)//2 + 1):		
+            rotateShell(grid, shell, n, m, k)
+        
+        return grid
+        
+        
+def rotateShell(arr, shell, n, m, r):
+
+    a = make1Darray(arr, shell, n, m)
+    a = rotate1Darray(a, r)
+    update2darray(arr, a, shell, n, m)
+
+# in this we make 1d array of the shell 
+# same as ring rotate and return that 1d array
+def make1Darray(arr, shell, n, m):
+
+    rmin = shell - 1
+    cmin = shell - 1
+    rmax = n - shell
+    cmax = m - shell
+
+    a1 = []
+    i = rmin
+    j = cmin
+
+    for _ in range(rmax - rmin):
+        a1.append(arr[i][j])
+        i+=1
+    
+    for _ in range(cmax - cmin):
+        a1.append(arr[i][j])
+        j+=1
+
+    for _ in range(rmax - rmin):
+        a1.append(arr[i][j])
+        i-=1
+
+    for _ in range(cmax - cmin):
+        a1.append(arr[i][j])
+        j-=1
+
+    # print(*a1)
+    return a1
+
+# in this we rotate our 1d array and return it
+def rotate1Darray(a, r):
+
+    n = len(a)
+    r = r % n
+    if r == 0 : return a		# if r = 0 no need to rotate
+
+    a = a[n - r - 1 : : -1] + a[ : n - r - 1 : -1] 
+    a = a[ :: -1]
+    
+    return a
+
+# in this we update our shell with the rotated 1d array in same matrix
+def update2darray(arr, a1, shell, n, m):
+
+    rmin = shell - 1
+    cmin = shell - 1
+    rmax = n - shell
+    cmax = m - shell
+
+    i = rmin
+    j = cmin
+
+    a1Index = 0
+
+    for _ in range(rmax - rmin):
+        arr[i][j] = a1[a1Index]
+        a1Index += 1
+        i+=1
+
+    
+    for _ in range(cmax - cmin):
+        arr[i][j] = a1[a1Index]
+        a1Index += 1
+        j+=1
+
+    for _ in range(rmax - rmin):
+        arr[i][j] = a1[a1Index]
+        a1Index += 1
+        i-=1
+
+    for _ in range(cmax - cmin):
+        arr[i][j] = a1[a1Index]
+        a1Index += 1
+        j-=1
+
+    # print(*a1)
+    return a1
+    
